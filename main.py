19c0d9d06fc7bd506303f2d3bf74d121102588f5
arefmokhtari <aref100am@gmail.com>
2022-05-30 18:19:11 +0430

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#                   [   Plague Dr.  ]
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+from pl import MyBot
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+api_id = 0
+api_hash = ''
+name = 'pl'
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+if __name__ == '__main__': 
+    bot = MyBot(name,api_id,api_hash)
+    bot.show()
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - #
